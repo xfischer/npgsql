16a278b0ca79acd729e3688dd5468bd080fdb439
zahidk <>
2010-09-03 18:37:51 +0000

    Case 16657:   Bump connectors version string to 8.4.4.14

--- EDBDataProvider2.0.2/src/EnterpriseDB.EDBClient/AssemblyInfo.cs
+++ EDBDataProvider2.0.2/src/EnterpriseDB.EDBClient/AssemblyInfo.cs
@@ -41,7 +41,7 @@ using System.Resources;
 [assembly : AssemblyDescription(".Net Data Provider for EnterpriseDB")]
 [assembly : AssemblyConfiguration("")]
 [assembly : AssemblyCompany("EnterpriseDB")]
-[assembly : AssemblyProduct("Postgres Plus Advanced Server 8.4 (8.4.4.10)")]
+[assembly : AssemblyProduct("Postgres Plus Advanced Server 8.4 (8.4.4.14)")]
 [assembly : AssemblyCopyright("The Npgsql Development Team")]
 [assembly : AssemblyTrademark("")]
 [assembly : AssemblyCulture("")]
@@ -54,7 +54,7 @@ using System.Resources;
 // You can specify all values by your own or you can build default build and revision
 // numbers with the '*' character (the default):
 
-[assembly : AssemblyVersion("8.4.4.10")]
+[assembly : AssemblyVersion("8.4.4.14")]
 
 
 
