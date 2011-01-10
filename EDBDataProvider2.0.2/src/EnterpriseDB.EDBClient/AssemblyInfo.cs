d581370865f8cf2c177c1b14b1a90c8b41b674eb
zahid.khan <>
2011-01-10 15:05:07 +0000

    Case 17505:   Bump version number for connectors

--- EDBDataProvider2.0.2/src/EnterpriseDB.EDBClient/AssemblyInfo.cs
+++ EDBDataProvider2.0.2/src/EnterpriseDB.EDBClient/AssemblyInfo.cs
@@ -41,7 +41,7 @@ using System.Resources;
 [assembly : AssemblyDescription(".Net Data Provider for EnterpriseDB")]
 [assembly : AssemblyConfiguration("")]
 [assembly : AssemblyCompany("EnterpriseDB")]
-[assembly : AssemblyProduct("Postgres Plus Advanced Server 8.4 (8.4.5.16)")]
+[assembly : AssemblyProduct("Postgres Plus Advanced Server 9.0 (9.0.0.2)")]
 [assembly : AssemblyCopyright("The Npgsql Development Team")]
 [assembly : AssemblyTrademark("")]
 [assembly : AssemblyCulture("")]
@@ -54,7 +54,7 @@ using System.Resources;
 // You can specify all values by your own or you can build default build and revision
 // numbers with the '*' character (the default):
 
-[assembly : AssemblyVersion("8.4.5.16")]
+[assembly : AssemblyVersion("9.0.0.2")]
 
 
 
--- edb/edb-dnd-provider/EDBDataProvider/EDBDataProvider/AssemblyInfo.cs
+++ edb/edb-dnd-provider/EDBDataProvider/EDBDataProvider/AssemblyInfo.cs
@@ -22,7 +22,7 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyCompany("EnterpriseDB")]
 
-[assembly: AssemblyProduct("Postgres Plus Advanced Server 8.4 (8.4.5.16)")]
+[assembly: AssemblyProduct("Postgres Plus Advanced Server 9.0 (9.0.0.2)")]
 
 [assembly: AssemblyCopyright("Portions Copyright (c) 2004-2006 - EnterpriseDB Corporation.  All Rights Reserved.")]
 
@@ -46,7 +46,7 @@ using System.Runtime.CompilerServices;
 
 
 
-[assembly: AssemblyVersion("8.4.5.16")]
+[assembly: AssemblyVersion("9.0.0.2")]
 
 
 
