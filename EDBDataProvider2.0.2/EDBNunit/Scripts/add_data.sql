f907859282a869d951ab5b7b7b21261b9f4426a6
ahmed.shinwari <>
2011-11-29 15:36:12 +0000

    Case 20191:   NUnit failures on Windows 32 & 64

--- EDBDataProvider2.0.2/EDBNunit/Scripts/add_data.sql
+++ EDBDataProvider2.0.2/EDBNunit/Scripts/add_data.sql
@@ -13,7 +13,7 @@ insert into tableA(field_text) values ('Random text');
 insert into tableA(field_int4) values (4);
 insert into tableA(field_int8) values (8);
 insert into tableA(field_bool) values (true);
-insert into tableA(field_text) values ('Text with \' single quote');
+insert into tableA(field_text) values (E'Text with \' single quote');
 insert into tableA(field_text) values ('Unicode test ã ó ê ñ ò ü');
 
 insert into tableB(field_int2) values (2);
