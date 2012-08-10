7a974013148b24d6cc5ada5a9140aee4c71ce052
talha.rizwan <>
2012-08-10 12:05:17 +0000

    Bumping Connector's version to 9.2.0.1

--- edb/edb-dnd-provider/EDBDataProvider/EDBDataProvider/AssemblyInfo.cs
+++ edb/edb-dnd-provider/EDBDataProvider/EDBDataProvider/AssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyCompany("EnterpriseDB")]
 
-[assembly: AssemblyProduct("Postgres Plus Advanced Server 9.1 (9.1.2.2)")]
+[assembly: AssemblyProduct("Postgres Plus Advanced Server 9.2 (9.2.0.1)")]
 
 [assembly: AssemblyCopyright("Portions Copyright (c) 2004-2012 - EnterpriseDB Corporation.  All Rights Reserved.")]
 
@@ -47,7 +47,7 @@ using System.Runtime.CompilerServices;
 
 
 
-[assembly: AssemblyVersion("9.1.2.2")]
+[assembly: AssemblyVersion("9.2.0.1")]
 
 
 
