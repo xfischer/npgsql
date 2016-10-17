6fa4c68a426ea3963f852a2df4648e970911622d
Hamid Quddus Akhtar <hamid.quddus@enterprisedb.com>
2016-10-17 18:22:58 +0500

    Bumping the version for 9.6 Beta2

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -16,6 +16,6 @@ using System.Resources;
 [assembly: NeutralResourcesLanguage("en", UltimateResourceFallbackLocation.MainAssembly)]
 
 // The following version attributes get rewritten by GitVersion as part of the build
-[assembly: AssemblyVersion("3.0.5.1")]
-[assembly: AssemblyFileVersion("3.0.5.1")]
-[assembly: AssemblyInformationalVersion("3.0.5.1")]
+[assembly: AssemblyVersion("3.0.5.2")]
+[assembly: AssemblyFileVersion("3.0.5.2")]
+[assembly: AssemblyInformationalVersion("3.0.5.2")]
