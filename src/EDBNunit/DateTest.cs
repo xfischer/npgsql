97116aeb09f870667d7ab68ec4f98b4d9a3c43a4
Umar Hayat <umar.hayat@enterprisedb.com>
2017-06-21 23:40:54 -0800

    1. Fixed,uncommented and added tests for Bytea.\n 2. Added more test for EDBDate Type

--- src/EDBNunit/DateTest.cs
+++ src/EDBNunit/DateTest.cs
@@ -2,6 +2,7 @@ using System;
 using NUnit.Framework;
 using EnterpriseDB.EDBClient;
 using System.Data;
+using EDBTypes;
 
 
 namespace DOTNET
@@ -15,8 +16,7 @@ namespace DOTNET
 	{
 		EDBConnection con;
 		bool testingSetDate = false;
-
-
+		
 		[SetUp]
 		public void Init()
 		{			
@@ -30,13 +30,12 @@ namespace DOTNET
 			TestUtil.dropTable(con, "testdate");
 			TestUtil.closeDB(con);
 		}
-			
+
 		[Test]
 		public void testGetDate() 
 		{
 
 			EDBCommand Command = new EDBCommand("",con);
-			
 
 			 //Statement stmt = con.createStatement();
 			Command.CommandText="insert into testdate values('1950-02-07')";
@@ -50,7 +49,6 @@ namespace DOTNET
 			Command.CommandText="insert into testdate values('1999-08-11')";
 			 a=Command.ExecuteNonQuery();
 			Assert.AreEqual(1,a);
-
 			
 			Command.CommandText="insert into testdate values('2001-02-13')";
 			 a=Command.ExecuteNonQuery();
@@ -64,12 +62,81 @@ namespace DOTNET
 			 a=Command.ExecuteNonQuery();
 			Assert.AreEqual(1,a);
 
-			
 			Command.CommandText="DELETE FROM " + "testdate";
 			 a=Command.ExecuteNonQuery();
 			Assert.AreEqual(6,a);
+		}
 
-	}
+		[Test]
+		public void ParseStringTest()
+		{
+			EDBDate date = EDBDate.Parse("2019-11-01");
+			Assert.AreEqual(2019, date.Year);
+			Assert.AreEqual(11, date.Month);
+			Assert.AreEqual(1, date.Day);
+
+			try
+			{
+				EDBDate.Parse("No Date");
+				Assert.Fail("Exception was expected with invalid date format");
+			}
+			catch ( FormatException )
+			{
+			}
+
+			EDBDate newDate = new EDBDate();
+			Assert.True(EDBDate.TryParse("2019-11-01", out newDate));
+			Assert.True(EDBDate.TryParse("2017-11-01", out newDate));
+
+			Assert.False(EDBDate.TryParse("11-01", out newDate));
+			Assert.False(EDBDate.TryParse("Total Random String", out newDate));
+		}
+
+		[Test]
+		public void AddTest()
+		{
+			EDBDate date = new EDBDate(2019, 11, 1);
+			Assert.AreEqual("2019-11-01", date.Add(new EDBTimeSpan(0, 0, 0)).ToString());
+			Assert.AreEqual("2019-12-01", date.Add(new EDBTimeSpan(1, 0, 0)).ToString());
+			Assert.AreEqual("2019-12-03", date.Add(new EDBTimeSpan(1, 2, 0)).ToString());
+			Assert.AreEqual("2020-01-01", date.Add(new EDBTimeSpan(2, 0, 0)).ToString());
+			Assert.AreEqual("2020-01-01", date.Add(new EDBTimeSpan(1, 31, 0)).ToString());
+
+			Assert.AreEqual("2020-01-01", date.AddDays(61).ToString());
+			Assert.AreEqual("2020-11-01", date.AddYears(1).ToString());
+			Assert.AreEqual("2019-12-01", date.AddMonths(1).ToString());
+
+			EDBDate date2 = new EDBDate(2012, 2, 1);
+			Assert.AreEqual("2012-02-01", date2.AddDays(0).ToString());
+			Assert.AreEqual("2012-02-29", date2.AddDays(28).ToString());
+			Assert.AreEqual("2012-03-01", date2.AddDays(29).ToString());
+		}
+
+		[Test]
+		public void ToStringTest()
+		{
+			Assert.AreEqual("2019-12-01", new EDBDate(2019, 12, 1).ToString());
+			Assert.AreEqual("2019-12-12", new EDBDate(2019, 12, 12).ToString());
+		}
+
+		[Test]
+		public void EqualTest()
+		{
+			EDBDate date1 = new EDBDate(2019, 12, 1);
+			EDBDate date2 = new EDBDate(2019, 12, 1);
+			EDBDate date3 = new EDBDate(2019, 11, 1);
+
+			Assert.AreEqual(date1, date2);
+			Assert.True(date1 == date2);
+			Assert.True(date1 != date3);
+			Assert.True(date1.Equals(date2));
+
+			Assert.AreNotEqual(date1, date3);
+			Assert.False(date1.Equals(date3));
+
+			Assert.AreEqual(0, date1.CompareTo(date2));
+			Assert.AreEqual(0, date1.Compare(date1, date2));
+		}
 
 	}
 }
