fcf983abd96bb495ee07d53aa5d90b62e027d670
Muhammad Aqeel <aqeel@Muhammads-MacBook-Pro-3.local>
2018-03-07 13:05:55 +0500

    Adjusted new directory structure of dotnet installer.

--- pkg/installer/build.bat
+++ pkg/installer/build.bat
@@ -15,6 +15,8 @@ mkdir %STAGING_DIR%\%TARGET_FRAMEWORK%
 mkdir %STAGING_DIR%\%TARGET_FRAMEWORK%\%FRAMEWORK_DEFINE%
 mkdir %STAGING_DIR%\%TARGET_FRAMEWORK%\%FRAMEWORK_DEFINE%1
 mkdir %STAGING_DIR%\%TARGET_FRAMEWORK%\netstandard1.3
+mkdir %STAGING_DIR%\%TARGET_FRAMEWORK%\EF
+mkdir %STAGING_DIR%\%TARGET_FRAMEWORK%\DDexProvider
 
 nuget install VSSDK.Shell.12
 nuget install AsyncRewriter -Version 0.6.0 -Output packages
@@ -43,9 +45,10 @@ nuget restore NpgsqlDdexProvider.sln
 
 msbuild.exe NpgsqlDdexProvider2010.csproj /p:Configuration=%RELEASE_CONFIGURATION% /p:%FRAMEWORK_DEFINE%=1 /p:Platform=%TARGET_PLATFORM% 
 
-copy bin\%RELEASE_CONFIGURATION%\EDBDdexProvider.dll %STAGING_DIR%\%TARGET_FRAMEWORK%
-copy bin\%RELEASE_CONFIGURATION%\EDBDdexProvider.vsix %STAGING_DIR%\%TARGET_FRAMEWORK%
-copy SSDLToPgSQL.tt %STAGING_DIR%\%TARGET_FRAMEWORK%
+copy bin\%RELEASE_CONFIGURATION%\EDBDdexProvider.dll %STAGING_DIR%\%TARGET_FRAMEWORK%\DDexProvider
+copy bin\%RELEASE_CONFIGURATION%\EDBDdexProvider.vsix %STAGING_DIR%\%TARGET_FRAMEWORK%\DDexProvider
+copy SSDLToPgSQL.tt %STAGING_DIR%\%TARGET_FRAMEWORK%\DDexProvider
+
 cd %SOURCE_PATH%
 cd src\EDBNunit
 msbuild.exe EDBNunit.csproj /p:Configuration=%RELEASE_CONFIGURATION% /p:%FRAMEWORK_DEFINE%=1 /p:Platform=%TARGET_PLATFORM% 
@@ -60,6 +63,7 @@ nuget restore packages.config -PackagesDirectory packages
 
 msbuild.exe EntityFramework6.Npgsql.csproj /p:Configuration=%RELEASE_CONFIGURATION% /p:%FRAMEWORK_DEFINE%=1 /p:Platform=%TARGET_PLATFORM% 
 
-copy bin\%RELEASE_CONFIGURATION%\EntityFramework6*.dll %STAGING_DIR%\%TARGET_FRAMEWORK%
-copy bin\%RELEASE_CONFIGURATION%\EntityFramework5*.dll %STAGING_DIR%\%TARGET_FRAMEWORK%
-copy %SOURCE_PATH%\src\NpgsqlDdexProvider\Resources\edb_logo.ico %STAGING_DIR%\%TARGET_FRAMEWORK%
+copy bin\%RELEASE_CONFIGURATION%\EntityFramework6*.dll %STAGING_DIR%\%TARGET_FRAMEWORK%\EF
+copy bin\%RELEASE_CONFIGURATION%\EntityFramework5*.dll %STAGING_DIR%\%TARGET_FRAMEWORK%\EF
+
+copy %SOURCE_PATH%\src\NpgsqlDdexProvider\Resources\edb_logo.ico %STAGING_DIR%\%TARGET_FRAMEWORK%\DDexProvider
