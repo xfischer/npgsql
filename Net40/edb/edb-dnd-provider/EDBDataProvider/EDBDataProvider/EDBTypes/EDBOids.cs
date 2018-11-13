850544f3523e6a8ecd3654bca4b3663762efd41c
Mahesh Bidve <mahesh.bidve@enterprisedb.com>
2018-11-13 11:35:27 +0530

    copied net40 code to master from V2_2_STABLE branch

---
+++ Net40/edb/edb-dnd-provider/EDBDataProvider/EDBDataProvider/EDBTypes/EDBOids.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace EDBOids
+{
+	/// <summary>
+	/// Summary description for EDBOids.
+	/// </summary>
+	
+		public enum ParamOid
+		{
+			int4 = 23
+		//
+			// TODO: Add constructor logic here
+			//
+		
+		}
+}	
