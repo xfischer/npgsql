6d118cd2b7fd5d66151f0501b811c62bad0b9467
Zahid Khan <zahid.khan@enterprisedb.com>
2020-03-10 18:46:10 +0500

    version update to 4.0.10.2

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -11,11 +11,11 @@ using System.Resources;
 [assembly: SecurityRules(SecurityRuleSet.Level1)]
 [assembly: AssemblyCompany(" EnterpriseDB.EDBClient Development Team")]
 [assembly: AssemblyProduct(" EnterpriseDB.EDBClient")]
-[assembly: AssemblyCopyright("Copyright © 2002 - 2017  EnterpriseDB.EDBClient Development Team")]
+[assembly: AssemblyCopyright("Copyright © 2002 - 2020  EnterpriseDB.EDBClient Development Team")]
 [assembly: AssemblyTrademark("")]
 [assembly: NeutralResourcesLanguage("en", UltimateResourceFallbackLocation.MainAssembly)]
 
 // The following version attributes get rewritten by GitVersion as part of the build
-[assembly: AssemblyVersion("4.0.10.1")]
-[assembly: AssemblyFileVersion("4.0.10.1")]
-[assembly: AssemblyInformationalVersion("4.0.10.1")]
+[assembly: AssemblyVersion("4.0.10.2")]
+[assembly: AssemblyFileVersion("4.0.10.2")]
+[assembly: AssemblyInformationalVersion("4.0.10.2")]
