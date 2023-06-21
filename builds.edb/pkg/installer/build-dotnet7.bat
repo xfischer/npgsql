f604ee1ece4f1339552b14e2552d9f35480bc679
Moazzum Ali <moazzum.ali@enterprisedb.com>
2023-06-21 13:13:59 +0500

    fixed net6 assemblies path and copy vb sample

--- builds.edb/pkg/installer/build-dotnet7.bat
+++ builds.edb/pkg/installer/build-dotnet7.bat
@@ -41,8 +41,8 @@ copy bin\%RELEASE_CONFIGURATION%\%FRAMEWORK_DEFINE%\EnterpriseDB.EDBClient.dll %
 copy bin\%RELEASE_CONFIGURATION%\%FRAMEWORK_DEFINE%\Microsoft.Extensions.Logging.Abstractions.dll %STAGING_DIR%\%TARGET_FRAMEWORK%\net7.0 || goto :error 
 
 mkdir %STAGING_DIR%\%TARGET_FRAMEWORK%\net6.0
-copy bin\%RELEASE_CONFIGURATION%\%FRAMEWORK_DEFINE%\EnterpriseDB.EDBClient.dll %STAGING_DIR%\%TARGET_FRAMEWORK%\net6.0 || goto :error 
-copy bin\%RELEASE_CONFIGURATION%\%FRAMEWORK_DEFINE%\Microsoft.Extensions.Logging.Abstractions.dll %STAGING_DIR%\%TARGET_FRAMEWORK%\net6.0 || goto :error 
+copy bin\%RELEASE_CONFIGURATION%\net6.0\EnterpriseDB.EDBClient.dll %STAGING_DIR%\%TARGET_FRAMEWORK%\net6.0 || goto :error 
+copy bin\%RELEASE_CONFIGURATION%\net6.0\Microsoft.Extensions.Logging.Abstractions.dll %STAGING_DIR%\%TARGET_FRAMEWORK%\net6.0 || goto :error 
 copy bin\%RELEASE_CONFIGURATION%\netstandard2.0\System.Runtime.CompilerServices.Unsafe.dll %STAGING_DIR%\%TARGET_FRAMEWORK%\net6.0 || goto :error
 
 mkdir %STAGING_DIR%\%TARGET_FRAMEWORK%\net5.0
